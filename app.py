68dcb0343b4a711b505f3b548664d49d542c3c94
lemonbits3-collab <lemonbits3@gmail.com>
2025-08-26 18:14:57 +0200

    Update app.py

--- app.py
+++ app.py
@@ -6,7 +6,7 @@ from datetime import datetime
 st.set_page_config(
     page_title="LemonPlanner",
     page_icon="🍋",
-    layout="centered",
+    layout="wide",   # <— HIER ändern
 )
 
 # ---- Heutiges Datum (Deutsch) ----
