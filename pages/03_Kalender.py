3dd77aa5c3d719b87af7abf4cf11df377ba10a90
lemonbits3-collab <lemonbits3@gmail.com>
2025-08-26 18:41:11 +0200

    Create 03_Kalender.py

---
+++ pages/03_Kalender.py
@@ -0,0 +1,6 @@
+import streamlit as st
+
+st.set_page_config(page_title="Kalender · LemonPlanner", page_icon="📅", layout="wide")
+
+st.title("📅 Kalender")
+st.info("Hier baust du später deinen Kalender ein.")
